c35254260e14052c14766fd4643c0a0558ad72f3
Todd Hancock <thancock20@hotmail.com>
2016-08-30 13:48:48 -0500

    Update home.jsx

--- client/modules/core/components/home.jsx
+++ client/modules/core/components/home.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
+import useSheet from 'react-jss';
 
-const Home = () => (
-  <div>
-    <h1>Mantra</h1>
-    <p>
-      Welcome to Mantra 0.2.0.
-    </p>
-    <ul>
-      <li>
-        Read <a target="_blank" href="https://kadirahq.github.io/mantra/">spec</a>
-      </li>
-      <li>
-        Learn <a target="_blank" href="https://github.com/sungwoncho/mantra-cli#commands">CLI</a>
-      </li>
-    </ul>
-  </div>
-);
+import jss from 'jss';
+import preset from 'jss-preset-default';
+jss.setup(preset());
 
-export default Home;
+const Home = ({ sheet }) => {
+
+  const { welcome } = sheet.classes;
+
+  return (
+    <div>
+      <h1>Mantra</h1>
+      <p className={welcome}>
+        Welcome to Mantra 0.2.0.
+      </p>
+      <ul>
+        <li>
+          Read <a target="_blank" href="https://kadirahq.github.io/mantra/">spec</a>
+        </li>
+        <li>
+          Learn <a target="_blank" href="https://github.com/sungwoncho/mantra-cli#commands">CLI</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+const styles = {
+  welcome: {
+    fontSize: '20px',
+    color: 'red'
+  }
+};
+
+export default useSheet(Home, styles);

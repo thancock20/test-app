a4785a4e445cffdcbf2bf507cfe1ff7a9d65af6a
Todd Hancock <thancock20@hotmail.com>
2016-09-08 13:02:47 -0500

    Fix merging issue with global styles, using object-merge

--- client/styles/index.js
+++ client/styles/index.js
@@ -1,7 +1,8 @@
+import objectMerge from 'object-merge';
 import normalize from './normalize';
 import styles from './styles.js';
 
-export default {
-  ...normalize,
-  ...styles
-};
+export default objectMerge(
+  normalize,
+  styles
+);

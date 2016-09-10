da1bf40d319d9985d59a2557cb8f0d2f43b3f68c
Todd Hancock <thancock20@hotmail.com>
2016-09-10 02:09:26 -0500

    Beautify normalize.js

--- client/styles/normalize.js
+++ client/styles/normalize.js
@@ -1,1 +1,146 @@
-export default {"html":{"font-family":"sans-serif","line-height":"1.15","-ms-text-size-adjust":"100%","-webkit-text-size-adjust":"100%"},"body":{"margin":"0"},"article, aside, details, figcaption, figure, footer, header, main, menu, nav, section, summary":{"display":"block"},"audio, canvas, progress, video":{"display":"inline-block"},"audio:not([controls])":{"display":"none","height":"0"},"progress":{"vertical-align":"baseline"},"template, [hidden]":{"display":"none"},"a":{"background-color":"transparent","-webkit-text-decoration-skip":"objects"},"a:active, a:hover":{"outline-width":"0"},"abbr[title]":{"border-bottom":"none","text-decoration":"underline dotted"},"b, strong":{"font-weight":"bolder"},"dfn":{"font-style":"italic"},"h1":{"font-size":"2em","margin":"0.67em 0"},"mark":{"background-color":"#ff0","color":"#000"},"small":{"font-size":"80%"},"sub, sup":{"font-size":"75%","line-height":"0","position":"relative","vertical-align":"baseline"},"sub":{"bottom":"-0.25em"},"sup":{"top":"-0.5em"},"img":{"border-style":"none"},"svg:not(:root)":{"overflow":"hidden"},"code, kbd, pre, samp":{"font-family":"monospace, monospace","font-size":"1em"},"figure":{"margin":"1em 40px"},"hr":{"box-sizing":"content-box","height":"0","overflow":"visible"},"button, input, optgroup, select, textarea":{"font":"inherit","margin":"0"},"optgroup":{"font-weight":"bold"},"button, input":{"overflow":"visible"},"button, select":{"text-transform":"none"},"button, html [type=\"button\"], [type=\"reset\"], [type=\"submit\"]":{"-webkit-appearance":"button"},"button::-moz-focus-inner, [type=\"button\"]::-moz-focus-inner, [type=\"reset\"]::-moz-focus-inner, [type=\"submit\"]::-moz-focus-inner":{"border-style":"none","padding":"0"},"button:-moz-focusring, [type=\"button\"]:-moz-focusring, [type=\"reset\"]:-moz-focusring, [type=\"submit\"]:-moz-focusring":{"outline":"1px dotted ButtonText"},"fieldset":{"border":"1px solid #c0c0c0","margin":"0 2px","padding":"0.35em 0.625em 0.75em"},"legend":{"box-sizing":"border-box","color":"inherit","display":"table","max-width":"100%","padding":"0","white-space":"normal"},"textarea":{"overflow":"auto"},"[type=\"checkbox\"], [type=\"radio\"]":{"box-sizing":"border-box","padding":"0"},"[type=\"number\"]::-webkit-inner-spin-button, [type=\"number\"]::-webkit-outer-spin-button":{"height":"auto"},"[type=\"search\"]":{"-webkit-appearance":"textfield","outline-offset":"-2px"},"[type=\"search\"]::-webkit-search-cancel-button, [type=\"search\"]::-webkit-search-decoration":{"-webkit-appearance":"none"},"::-webkit-input-placeholder":{"color":"inherit","opacity":"0.54"},"::-webkit-file-upload-button":{"-webkit-appearance":"button","font":"inherit"}}
+export default {
+  html: {
+    fontFamily: 'sans-serif',
+    lineHeight: '1.15',
+    MsTextSizeAdjust: '100%',
+    WebkitTextSizeAdjust: '100%'
+  },
+  body: {
+    margin: '0'
+  },
+  'article, aside, details, figcaption, figure, footer, header, main, menu, nav, section, summary': {
+    display: 'block'
+  },
+  'audio, canvas, progress, video': {
+    display: 'inline-block'
+  },
+  'audio:not([controls])': {
+    display: 'none',
+    height: '0'
+  },
+  progress: {
+    verticalAlign: 'baseline'
+  },
+  'template, [hidden]': {
+    display: 'none'
+  },
+  a: {
+    backgroundColor: 'transparent',
+    WebkitTextDecorationSkip: 'objects'
+  },
+  'a:active, a:hover': {
+    outlineWidth: '0'
+  },
+  'abbr[title]': {
+    borderBottom: 'none',
+    textDecoration: 'underline dotted'
+  },
+  'b, strong': {
+    fontWeight: 'bolder'
+  },
+  dfn: {
+    fontStyle: 'italic'
+  },
+  h1: {
+    fontSize: '2em',
+    margin: '0.67em 0'
+  },
+  mark: {
+    backgroundColor: '#ff0',
+    color: '#000'
+  },
+  small: {
+    fontSize: '80%'
+  },
+  'sub, sup': {
+    fontSize: '75%',
+    lineHeight: '0',
+    position: 'relative',
+    verticalAlign: 'baseline'
+  },
+  sub: {
+    bottom: '-0.25em'
+  },
+  sup: {
+    top: '-0.5em'
+  },
+  img: {
+    borderStyle: 'none'
+  },
+  'svg:not(:root)': {
+    overflow: 'hidden'
+  },
+  'code, kbd, pre, samp': {
+    fontFamily: 'monospace, monospace',
+    fontSize: '1em'
+  },
+  figure: {
+    margin: '1em 40px'
+  },
+  hr: {
+    boxSizing: 'content-box',
+    height: '0',
+    overflow: 'visible'
+  },
+  'button, input, optgroup, select, textarea': {
+    font: 'inherit',
+    margin: '0'
+  },
+  optgroup: {
+    fontWeight: 'bold'
+  },
+  'button, input': {
+    overflow: 'visible'
+  },
+  'button, select': {
+    textTransform: 'none'
+  },
+  'button, html [type=\'button\'], [type=\'reset\'], [type=\'submit\']': {
+    WebkitAppearance: 'button'
+  },
+  'button::-moz-focus-inner, [type=\'button\']::-moz-focus-inner, [type=\'reset\']::-moz-focus-inner, [type=\'submit\']::-moz-focus-inner': {
+    borderStyle: 'none',
+    padding: '0'
+  },
+  'button:-moz-focusring, [type=\'button\']:-moz-focusring, [type=\'reset\']:-moz-focusring, [type=\'submit\']:-moz-focusring': {
+    outline: '1px dotted ButtonText'
+  },
+  fieldset: {
+    border: '1px solid #c0c0c0',
+    margin: '0 2px',
+    padding: '0.35em 0.625em 0.75em'
+  },
+  legend: {
+    boxSizing: 'border-box',
+    color: 'inherit',
+    display: 'table',
+    maxWidth: '100%',
+    padding: '0',
+    whiteSpace: 'normal'
+  },
+  textarea: {
+    overflow: 'auto'
+  },
+  '[type=\'checkbox\'], [type=\'radio\']': {
+    boxSizing: 'border-box',
+    padding: '0'
+  },
+  '[type=\'number\']::-webkit-inner-spin-button, [type=\'number\']::-webkit-outer-spin-button': {
+    height: 'auto'
+  },
+  '[type=\'search\']': {
+    WebkitAppearance: 'textfield',
+    outlineOffset: '-2px'
+  },
+  '[type=\'search\']::-webkit-search-cancel-button, [type=\'search\']::-webkit-search-decoration': {
+    WebkitAppearance: 'none'
+  },
+  '::-webkit-input-placeholder': {
+    color: 'inherit',
+    opacity: '0.54'
+  },
+  '::-webkit-file-upload-button': {
+    WebkitAppearance: 'button',
+    font: 'inherit'
+  }
+};
--- client/styles/styles.js
+++ client/styles/styles.js
@@ -4,8 +4,7 @@ const secondaryColor = '#0088FF';
 export default {
   h1: {
     fontSize: 64,
-    color: primaryColor,
-    display: 'flex'
+    color: primaryColor
   },
   ul: {
     color: secondaryColor
